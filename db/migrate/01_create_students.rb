87f8ff12fb94c08ce817e9940fcf981e365dbcc0
kellyeryan <kellyeryan@gmail.com>
2019-10-04 10:06:05 -0400

    Finish writing migrations lab

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,20 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+  # connection = ActiveRecord::Base.establish_connection(
+  # :adapter => "sqlite3",
+  # :database => "db/students.sqlite")
+
+  # sql = <<-SQL
+  #   CREATE TABLE IF NOT EXISTS students (
+  #   id INTEGER PRIMARY KEY,
+  #   name TEXT,
+  #   )
+  # SQL
+
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
+
 end
